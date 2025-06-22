cdb5382e77ac5360e8d361aeee4cfd76695b7c3d
ksimmy1234 <112470623+ksimmy1234@users.noreply.github.com>
2025-06-22 15:59:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+let btn = document.querySelector("button");
+let gameseq=[];
+let userseq = [];
+let highScore = 0;
+let startBtn = document.getElementById("start-btn");
+
+let btns = ["red","green","yellow","blue"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("game started");
+        started = true;
+
+        Levelup();
+    }
+});
+
+startBtn.addEventListener("click", function () {
+    if (!started) {
+        started = true;
+        Levelup();
+        startBtn.style.display = "none"; // hide button after click
+    }
+});
+
+
+function Levelup(){
+    userseq = [];
+    level++;
+    h2.innerText = `Level ${level}`;  //It will change the value of h2 innertext to new level and will print that
+    
+    let randomIndex = Math.floor(Math.random()*3);
+    let randColour = btns[randomIndex];
+    let randBtn = document.querySelector(`.${randColour}`);
+    // console.log(randColour);
+    // console.log(randBtn);
+    gameseq.push(randColour);
+    console.log(gameseq);
+    gameflash(randBtn);
+}
+
+function gameflash(btn){
+    btn.classList.add("gameflash");
+    setTimeout(function(){
+        btn.classList.remove("gameflash");
+    },250);
+}
+
+function userflash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },500);
+}
+
+function btnPress(){
+    let btn = this;
+    userflash(btn);
+
+    userColor = btn.getAttribute("id");
+    userseq.push(userColor);
+
+    checkAns(userseq.length-1);
+}
+
+function checkAns(idx) {
+    if(userseq[idx] == gameseq[idx]){
+        if(userseq.length == gameseq.length){
+            setTimeout(Levelup,1000);
+        }
+    }
+    else{
+        if(level > highScore){
+            highScore = level;
+        }
+        h2.innerHTML = `Game Over! Your score was <b>${level}\u{1F389}</b> <br> Highest Score: <b>${highScore}</b> . Press any key to start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "#333";
+        },150)
+        reset();
+        resetBtn();
+    }
+
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
+}
+
+function resetBtn() {
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
+
+    // Show button again and change text to Restart Game
+    startBtn.innerText = "Restart Game";
+    startBtn.style.display = "inline-block";
+}
+
+
